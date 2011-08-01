75ebf1df71d46eabb4757194e2693a13dec81d61
Rune Lind <rulind@gmail.com>
2011-08-01 19:57:39 +0200

    update

--- spec/solrIndex_manager_spec.rb
+++ spec/solrIndex_manager_spec.rb
@@ -8,11 +8,11 @@ describe SolrIndexManager do
       def `(cmd)
         itr = (1..8)
         if cmd.start_with? 'hadoop fs -du'
-          return itr.map { |n| "#{n*10000000000} solrindex/test_index/part-r-0000#{n}" }.join "\n"
+          return itr.map { |n| "#{n*10000000000} solrindex/test_index/part-r-#{n}" }.join "\n"
         elsif cmd.start_with? 'hadoop fs -cat'
-          return "key '2011#{/\d+/.match(cmd).to_s.to_i}0': 62700332 documents"
+          return "key '#{/\d+/.match(cmd).to_s.to_i}': 62700332 documents"
         elsif cmd.start_with? 'ls /merge_to/test'
-          return itr.map {|n| "20110#{n}0"}.join("\n")
+          return itr.map {|n| "#{n}"}.join("\n")
         elsif cmd.start_with? 'du -k'
           return "20110104/t2342"
         else
