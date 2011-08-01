6cf4b5aa5d6d428332c1d90d990b8d4b128440b2
Rune Lind <rulind@gmail.com>
2011-08-01 18:51:28 +0200

    update

--- spec/solrIndex_manager_spec.rb
+++ spec/solrIndex_manager_spec.rb
@@ -6,13 +6,13 @@ describe SolrIndexManager do
     Readline.stub(:readline).and_return 'y'
     module Kernel
       def `(cmd)
+        itr = (1..8)
         if cmd.start_with? 'hadoop fs -du'
-          m = (1..4).map { |n| "#{n*10000000000}  solrindex/test_index/part-r-0000#{n}" }
-          return m.join "\n"
+          return itr.map { |n| "#{n*10000000000} solrindex/test_index/part-r-0000#{n}" }.join "\n"
         elsif cmd.start_with? 'hadoop fs -cat'
-          return "key '201101': 62700332"
+          return "key '2011#{/\d+/.match(cmd).to_s.to_i}0': 62700332 documents"
         elsif cmd.start_with? 'ls /merge_to/test'
-          return "20110101\n20110102\n20110103\n20110104"
+          return itr.map {|n| "20110#{n}0"}.join("\n")
         elsif cmd.start_with? 'du -k'
           return "20110104/t2342"
         else
